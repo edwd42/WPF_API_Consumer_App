155b3088b16af8fa6e3da624d99d0ab994dbd8fe
edward.dupre <edward.dupre@redcross.org>
2021-08-15 21:07:43 -0400

    add method ApiClient.InitializeClient()

--- ClassLibrary/ApiHelper.cs
+++ ClassLibrary/ApiHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,13 @@ namespace ClassLibrary
 {
     public class ApiHelper
     {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
     }
 }
