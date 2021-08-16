d423f42014f208c3832187a4f0b7faa21d8c99e3
edward.dupre <edward.dupre@redcross.org>
2021-08-15 22:40:23 -0400

    add class ComicProcessor.cs

--- ClassLibrary/ComicProcessor.cs
+++ ClassLibrary/ComicProcessor.cs
@@ -20,6 +20,20 @@ namespace ClassLibrary
             {
                 url = "https://xkcd.com/info.0.json";
             }
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    ComicModel comic = await response.Content.ReadAsAsync<ComicModel>();
+
+                    return comic;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
         }
     }
 }
